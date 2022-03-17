7c9207a8a481d0ab910e3a8b377823d99dbbc069
Miłosz Brzechczyn <miloszbrzechczyn@gmail.com>
2022-03-17 08:05:39 +0100

    First commit. Fix all those ugly bastards.

--- src/Application.ts
+++ src/Application.ts
@@ -3,6 +3,12 @@ import * as Framework from "./framework/BaseApplication";
 
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+// Ugly, very ugly, move it somewhere else.
+const LEFT_ARROW:  string = "ArrowLeft";
+const UP_ARROW:    string = "ArrowUp";
+const RIGHT_ARROW: string = "ArrowRight";
+const DOWN_ARROW:  string = "ArrowDown";
+
 namespace Utility {
 
     export const createWireframe = (geometry: THREE.BufferGeometry, color: THREE.ColorRepresentation, width: number): THREE.LineSegments => {
@@ -35,12 +41,9 @@ class Application extends Framework.BaseApplication {
     private readonly scene:  THREE.Scene;
     private readonly camera: THREE.PerspectiveCamera;
 
-    private readonly cubeMesh: THREE.Mesh;
+    private readonly plane: THREE.Mesh;
 
-    private readonly ambientLight: THREE.AmbientLight;
-    private readonly pointLight:   THREE.PointLight;
-
-    private readonly pointLightHelper: THREE.PointLightHelper;
+    private readonly player: THREE.Mesh;
 
     private readonly orbitControls: OrbitControls;
 
@@ -60,72 +63,69 @@ class Application extends Framework.BaseApplication {
 
         super();
 
+        // Move all those initialization things to seperate methods
+        // in order to keep the code clean.
+
         this.scene = new THREE.Scene();
-        this.scene.add(new THREE.GridHelper(10, 10));
 
         this.camera = new THREE.PerspectiveCamera(75, (innerWidth / innerHeight), 0.1, 1000);
-        this.camera.position.set(0, 1, 4);
-        this.camera.lookAt(0, 1, 0);
+        this.camera.position.set(0, 10, 0);
+        this.camera.lookAt(0, 0, 0);
+        this.camera.name = "main-camera";
         this.scene.add(this.camera);
 
-        { // cubes setup
-            this.cubeMesh = new THREE.Mesh(
-                new THREE.BoxGeometry(1.0, 1.0, 1.0),
-                new THREE.MeshPhongMaterial({
-                    color:    new THREE.Color(1.0, 1.0, 0.0),
-                    specular: new THREE.Color(0.1, 0.1, 0.1),
-                }),
-            );
-            this.cubeMesh.position.set(0, 1, 0);
-            this.scene.add(this.cubeMesh);
+        this.plane = new THREE.Mesh(
+            new THREE.PlaneGeometry(10, 10, 10, 10),
+            new THREE.MeshNormalMaterial({
+                side: THREE.DoubleSide,
+            }),
+        );
+        this.plane.rotation.set(THREE.MathUtils.degToRad(90), 0, 0);
+        this.plane.name = "main-plane";
+        this.scene.add(this.plane);
 
-            Utility.addWireframe(this.cubeMesh, 0x0, 4);
+        // Remove that thing later. Maybe.
+        Utility.addWireframe(this.plane, 0x0, 2);
 
-            const smallerCube = new THREE.Mesh(
-                new THREE.BoxGeometry(0.5, 0.5, 0.5),
-                new THREE.MeshPhongMaterial({
-                    color: new THREE.Color(1.0, 0.0, 0.0),
-                }),
-            );
+        this.player = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({
+                color: 0xff0000,
+            }),
+        );
+        this.player.position.set(0, 0.5, 0);
+        this.player.name = "player";
+        this.scene.add(this.player);
 
-            smallerCube.position.set(0.5, 0.5, 0.5);
-            Utility.addWireframe(smallerCube, 0x0, 4);
-            this.cubeMesh.add(smallerCube);
-        }
-
-        { // lighting setup
-            this.ambientLight = new THREE.AmbientLight(new THREE.Color(1.0, 1.0, 1.0), 0.8);
-            this.scene.add(this.ambientLight);
-
-            this.pointLight = new THREE.PointLight(new THREE.Color(1.0, 1.0, 1.0), 2.0);
-            this.pointLight.position.set(0, 1, 4);
-            this.scene.add(this.pointLight);
-
-            this.pointLightHelper = new THREE.PointLightHelper(this.pointLight);
-            this.scene.add(this.pointLightHelper);
-        }
-
-        { // orbit controls setup
-            this.orbitControls = new OrbitControls(this.camera, this.renderer.domElement);
-            this.orbitControls.target.y = 1;
-            this.orbitControls.update();
-
-            this.orbitControls.enablePan  = false;
-            this.orbitControls.enableZoom = true;
-        }
+        this.orbitControls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.orbitControls.enablePan = false;
 
         //Hey, it's too dangerous here, move out!
         window.addEventListener("resize", this.updateCamera);
+
+        //Very bad solution! Move it to update method in order to make use of deltatime!
+        // + you add GLOBAL event listener to the entire document.
+        // + it is desynchronised with game's loop (update & render). Too bad!
+        //FIXME: Fix that!
+        document.addEventListener("keydown", (event: KeyboardEvent) => {
+
+            const playerSpeed = 0.3;
+
+            switch (event.key) {
+
+            case LEFT_ARROW:  this.player.position.x -= playerSpeed; break;
+            case RIGHT_ARROW: this.player.position.x += playerSpeed; break;
+            case UP_ARROW:    this.player.position.z -= playerSpeed; break;
+            case DOWN_ARROW:  this.player.position.z += playerSpeed; break;
+
+            }
+
+        });
 
     }
 
     protected update: Framework.UpdateMethod = (deltaTime: number, totalTime: number) => {
 
-        this.pointLight.position.x = 3 * Math.sin(totalTime / 1000);
-        this.pointLight.position.z = 3 * Math.cos(totalTime / 1000);
-
-        this.cubeMesh.rotation.x += deltaTime;
-        this.cubeMesh.rotation.z += deltaTime;
 
     }
 
